ded033dbe4ba1676d193ce88a84cd124485a63aa
Luke Berry <lukeberrypi@gmail.com>
2026-06-25 01:47:03 +0100

    deployy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://lukeberrypi.com",
+  server: {
+    allowedHosts: ["lukeberrypi.com", "www.lukeberrypi.com"],
+  },
   markdown: {
     shikiConfig: {
       themes: { light: "github-light", dark: "github-dark" },
